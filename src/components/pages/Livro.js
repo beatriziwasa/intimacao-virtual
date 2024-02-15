e8788adf7a614d2aebaf190745d7fba69569402e
tiago.iwasa <tiago.iwasa@dataprev.gov.br>
2024-02-14 22:30:49 -0300

    somatorio dos livros

--- src/components/pages/Livro.js
+++ src/components/pages/Livro.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useParams } from "react-router-dom";
 import { Select, MenuItem, Box } from '@mui/material/';
+import { GoogleAPI } from '../api/GoogleAPI';
 import livroRegistro from '../../images/livro-registro.jpg';
 import logoPCSC from '../../images/logo-policial-civil.png';
 import SidebarMenu from '../layout/SidebarMenu';
 
 export const Livro = (props) => {
+
+    useEffect(() => {
+        obterTotaisIPs(ano);
+    }, []);
+
     const { proc } = useParams();
     const [tipoProcedimento, setTipoProcedimento] = React.useState(proc);
     const [ano, setAno] = React.useState("2024");
+    const [qtdIPsTramitando, setQtdIPsTramitando] = React.useState(0);
+    const [qtdIPsRemetidos, setQtdIPsRemetidos] = React.useState(0);
+    const [qtdIPsBaixados, setQtdIPsBaixados] = React.useState(0);
+    const [qtdIPsArquivados, setQtdIPsArquivados] = React.useState(0);
+    const [qtdTCsTramitando, setQtdTCsTramitando] = React.useState(0);
+    const [qtdTCsRemetidos, setQtdTCsRemetidos] = React.useState(0);
+    const [qtdTCsBaixados, setQtdTCsBaixados] = React.useState(0);
+    const [qtdTCsArquivados, setQtdTCsArquivados] = React.useState(0);
+    const [qtdAPFsTramitando, setQtdAPFsTramitando] = React.useState(0);
+    const [qtdAPFsRemetidos, setQtdAPFsRemetidos] = React.useState(0);
+    const [qtdAPFsBaixados, setQtdAPFsBaixados] = React.useState(0);
+    const [qtdAPFsArquivados, setQtdAPFsArquivados] = React.useState(0);
     const link = "/" + tipoProcedimento + "/" + ano;
     
     const tiposProcedimentos = [
@@ -59,10 +77,123 @@ export const Livro = (props) => {
 
     const handleChangeSelectTipoProcedimento = (event) => {
         setTipoProcedimento(event.target.value);
+        if (event.target.value === "ip") {
+            obterTotaisIPs(ano);
+        } else if (event.target.value === "tc") {
+            obterTotaisTCs(ano);
+        } else if (event.target.value === "apf") {
+            obterTotaisAPFs(ano);
+        }
     }
 
     const handleChangeSelectAno = (event) => {
         setAno(event.target.value);
+        if (tipoProcedimento === "ip") {
+            obterTotaisIPs(event.target.value);
+        } else if (tipoProcedimento === "tc") {
+            obterTotaisTCs(event.target.value);
+        } else if (tipoProcedimento === "apf") {
+            obterTotaisAPFs(event.target.value);
+        }
+    }
+
+    const obterTotaisIPs = (ano) => {
+        GoogleAPI.consultar('IP').then((ip) => {
+            let qtdIPsTramitando = 0;
+            let qtdIPsRemetidos = 0;
+            let qtdIPsBaixados = 0;
+            let qtdIPsArquivados = 0;
+            
+            for (let i = 0; i < ip.length; i++) {
+                if (ip[i].status === "Tramitando") {
+                    qtdIPsTramitando = qtdIPsTramitando + 1;
+                    
+                } else if(ip[i].status === "Remetido") {
+                    if (ip[i].dataRemessa.split("-")[0] === ano) {
+                        qtdIPsRemetidos = qtdIPsRemetidos + 1
+                    }
+
+                } else if(ip[i].status === "Baixado") {
+                    qtdIPsBaixados = qtdIPsBaixados + 1
+
+                } else if(ip[i].status === "Arquivado") {
+                    if (ip[i].dataRemessa.split("-")[0] === ano) {
+                        qtdIPsArquivados = qtdIPsArquivados + 1
+                    }
+                }
+            }
+
+            setQtdIPsTramitando(qtdIPsTramitando);
+            setQtdIPsRemetidos(qtdIPsRemetidos);
+            setQtdIPsBaixados(qtdIPsBaixados);
+            setQtdIPsArquivados(qtdIPsArquivados);
+        });
+    }
+
+    const obterTotaisTCs = (ano) => {
+        GoogleAPI.consultar('TC').then((tc) => {
+            let qtdTCsTramitando = 0;
+            let qtdTCsRemetidos = 0;
+            let qtdTCsBaixados = 0;
+            let qtdTCsArquivados = 0;
+            
+            for (let i = 0; i < tc.length; i++) {
+                if (tc[i].status === "Tramitando") {
+                    qtdTCsTramitando = qtdTCsTramitando + 1;
+
+                } else if(tc[i].status === "Remetido") {
+                    if (tc[i].dataRemessa.split("-")[0] === ano) {
+                        qtdTCsRemetidos = qtdTCsRemetidos + 1
+                    }
+
+                } else if(tc[i].status === "Baixado") {
+                    qtdTCsBaixados = qtdTCsBaixados + 1
+
+                } else if(tc[i].status === "Arquivado") {
+                    if (tc[i].dataRemessa.split("-")[0] === ano) {
+                        qtdTCsArquivados = qtdTCsArquivados + 1
+                    }
+                }
+            }
+
+            setQtdTCsTramitando(qtdTCsTramitando);
+            setQtdTCsRemetidos(qtdTCsRemetidos);
+            setQtdTCsBaixados(qtdTCsBaixados);
+            setQtdTCsArquivados(qtdTCsArquivados);
+        });
+    }
+
+    const obterTotaisAPFs = (ano) => {
+        GoogleAPI.consultar('APF').then((apf) => {
+            let qtdAPFsTramitando = 0;
+            let qtdAPFsRemetidos = 0;
+            let qtdAPFsBaixados = 0;
+            let qtdAPFsArquivados = 0;
+            
+            for (let i = 0; i < apf.length; i++) {
+                if (apf[i].status === "Tramitando") {
+                    qtdAPFsTramitando = qtdAPFsTramitando + 1;
+
+                } else if(apf[i].status === "Remetido") {
+                    if (apf[i].dataRemessa.split("-")[0] === ano) {
+                        qtdAPFsRemetidos = qtdAPFsRemetidos + 1
+                    }
+
+                } else if(apf[i].status === "Baixado") {
+                    qtdAPFsBaixados = qtdAPFsBaixados + 1
+
+                } else if(apf[i].status === "Arquivado") {
+                    if (apf[i].dataRemessa.split("-")[0] === ano) {
+                        qtdAPFsArquivados = qtdAPFsArquivados + 1
+                    }
+                }
+            }
+
+            setQtdAPFsTramitando(qtdAPFsTramitando);
+            setQtdAPFsRemetidos(qtdAPFsRemetidos);
+            setQtdAPFsBaixados(qtdAPFsBaixados);
+            setQtdAPFsArquivados(qtdAPFsArquivados);
+        });
     }
 
     return (
@@ -106,6 +237,57 @@ export const Livro = (props) => {
                     <Box sx={{ marginTop: '50px' }}>
                         <Link to={link}><img alt="Logo da PCSC" src={logoPCSC} width="100" height="120" /></Link>
                     </Box>
+
+                    { tipoProcedimento === "ip" ?
+                    <>
+                    <Box sx={{ marginTop: '20px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="tramitando">Tramitando: {qtdIPsTramitando}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="remetidos">Remetidos ({ano}): {qtdIPsRemetidos}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="baixados">Baixados: {qtdIPsBaixados}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="arquivados">Arquivados ({ano}): {qtdIPsArquivados}</label>
+                    </Box>
+                    </>
+                    : undefined }
+                    
+                    { tipoProcedimento === "tc" ?
+                    <>
+                    <Box sx={{ marginTop: '20px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="tramitando">Tramitando: {qtdTCsTramitando}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="remetidos">Remetidos ({ano}): {qtdTCsRemetidos}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="baixados">Baixados: {qtdTCsBaixados}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="arquivados">Arquivados ({ano}): {qtdTCsArquivados}</label>
+                    </Box>
+                    </>
+                    : undefined }
+                    
+                    { tipoProcedimento === "apf" ?
+                    <>
+                    <Box sx={{ marginTop: '20px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="tramitando">Tramitando: {qtdAPFsTramitando}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="remetidos">Remetidos ({ano}): {qtdAPFsRemetidos}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="baixados">Baixados: {qtdAPFsBaixados}</label>
+                    </Box>
+                    <Box sx={{ marginTop: '10px', fontSize: '15px', color: 'white', fontWeight: 'bold' }}>
+                        <label htmlFor="arquivados">Arquivados ({ano}): {qtdAPFsArquivados}</label>
+                    </Box>
+                    </>
+                    : undefined }
 
                     <SidebarMenu openDrawer={props.openDrawer} setOpenDrawer={props.setOpenDrawer} />
                 </Box>
