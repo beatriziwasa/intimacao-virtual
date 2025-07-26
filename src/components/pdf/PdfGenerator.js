3b7fbe537cf20e0e5e775ae9e278aedaabff48c0
tiago.iwasa <tiago.iwasa@dataprev.gov.br>
2025-07-26 18:24:14 -0300

    Add html2canvas p/ melhorar certidao

--- src/components/pdf/PdfGenerator.js
+++ src/components/pdf/PdfGenerator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 import _ from 'lodash';
 import moment from 'moment';
 import 'moment/locale/pt';
@@ -33,65 +34,69 @@ const PdfGenerator = (intimacao, tipo) => {
     // Add images and text to the PDF
     const imageUrl = logo;
     pdf.addImage(imageUrl, 'PNG', 20, 10, 25, 30);
-    pdf.setFontSize(10);
-    pdf.setFont('Arial');
+    pdf.setFont('Helvetica');
+    pdf.setFontSize(9);
     pdf.text('ESTADO DE SANTA CATARINA', 50, 15);
     pdf.text('SECRETARIA DE ESTADO DA SEGURANÇA PÚBLICA', 50, 20);
     pdf.text('DELEGACIA GERAL DA POLÍCIA CIVIL', 50, 25);
     pdf.text('Delegacia de Proteção ao Turista - DPTUR - AEROPORTO', 50, 30);
     pdf.text('Rodovia de Acesso ao Aeroporto, 6200, Carianos - FLORIANÓPOLIS/SC (48)3665-5726', 50, 35);
-    pdf.setFontSize(16);
-    pdf.text('Certidão', 90, 100);
+    
+    let mensagem = "";
     
     if (tipo) {
-        pdf.setFontSize(10);
+        mensagem = `Certifico que, no dia ${moment(new Date()).format('L')}, foi intimado via WhatsApp a pessoa de ${intimacao.nome},\n
+        através do telefone ${intimacao.telefone}, sendo agendada sua oitiva para o dia ${formatDateHour(intimacao.dataAudiencia, intimacao.horaAudiencia)}.\n
+        Eu,______________, que o digitei.`;
+    } else {
+        mensagem = `Certifico que, no dia ${formatDateHour(intimacao.dataAudiencia, intimacao.horaAudiencia)}, a pessoa intimada ${intimacao.nome}\n
+        não se apresentou de forma presencial ou virtual para o ato designado.\n
+        Eu,______________, que o digitei.`;
+    }
+
+    let htmlString = `
+    <div style="border: solid; border-width: 2px; width: 450px">
+        <p style="margin-top: 20px; text-transform: uppercase; text-align: center; font-size: 12">Certidão</p>
+        <br/>
+        <p style="margin: 20px; font-family: Helvetica; font-size: 9; text-align: justify">${mensagem}</p>
+        <br/>
+        <p style="margin: 20px; text-align: center">FLORIANÓPOLIS, ${moment(new Date()).format('LL')}</p>
+    </div>`;
+    
+    // Create a temporary div element
+    const tempDiv = document.createElement('div');
+    tempDiv.style.position = 'absolute'; // Prevent it from affecting layout visually
+    tempDiv.style.left = '-9999px'; // Move it off-screen
+    document.body.appendChild(tempDiv);
+
+    // Set the innerHTML of the temporary div to your HTML string
+    tempDiv.innerHTML = htmlString;
+
+    html2canvas(tempDiv).then((canvas) => {
+        // Append the generated canvas to the body or another desired location
+        document.body.appendChild(canvas);
+
+        // Remove the temporary div
+        document.body.removeChild(tempDiv);
         
-        let mensagem = `Certifico que, no dia %DATA_HOJE%, foi intimado via WhatsApp a pessoa de %NOME_INTIMADO%,\natravés do telefone %TELEFONE_INTIMADO%, sendo agendada sua oitiva para o dia %DATA_HORA_OITIVA%.\nEu,______________, que o digitei.`;
-        mensagem = mensagem.replaceAll("%DATA_HOJE%", moment(new Date()).format('L'));
-        mensagem = mensagem.replaceAll("%NOME_INTIMADO%", intimacao.nome);
-        mensagem = mensagem.replaceAll("%TELEFONE_INTIMADO%", intimacao.telefone);
-        mensagem = mensagem.replaceAll("%DATA_HORA_OITIVA%", formatDateHour(intimacao.dataAudiencia, intimacao.horaAudiencia));
-        
-        pdf.text(mensagem, 20, 120);
-        
-        let dataLocal = `FLORIANÓPOLIS, %DATA_LOCAL%`;
-        dataLocal = dataLocal.replaceAll("%DATA_LOCAL%", moment(new Date()).format('LL'));
-        pdf.text(dataLocal, 70, 145);
-        
+        const img = canvas.toDataURL("image/png");
+        pdf.addImage(img, 'JPEG', 45, 60);
+
         // Set document properties
-        pdf.setProperties({
-            title: "Certidão da Intimação Realizada com Sucesso"
-        })
+        if (tipo) {
+            pdf.setProperties({title: "Certidão da Intimação Realizada com Sucesso"});
+        } else {
+            pdf.setProperties({title: "Certidão de Não Comparecimento do Intimado"})
+        }
         
         // Save the PDF
         pdf.save(`CertidaoIntimacaoRealizadaComSucesso.pdf`);
-    
-    } else {
-        pdf.setFontSize(10);
 
-        let mensagem = `Certifico que, no dia %DATA_HORA_OITIVA%, a pessoa intimada %NOME_INTIMADO%\nnão se apresentou de forma presencial ou virtual para o ato designado.\nEu,______________, que o digitei.`;
-        mensagem = mensagem.replaceAll("%NOME_INTIMADO%", intimacao.nome);
-        mensagem = mensagem.replaceAll("%DATA_HORA_OITIVA%", formatDateHour(intimacao.dataAudiencia, intimacao.horaAudiencia));
-        
-        pdf.text(mensagem, 20, 120);
-
-        let dataLocal = `FLORIANÓPOLIS, %DATA_LOCAL%`;
-        dataLocal = dataLocal.replaceAll("%DATA_LOCAL%", moment(new Date()).format('LL'));
-        pdf.text(dataLocal, 70, 145);
-
-        // Set document properties
-        pdf.setProperties({
-            title: "Certidão de Não Comparecimento do Intimado"
-        })
-        
-        // Save the PDF
-        pdf.save(`CertidaoNaoComparecimentoIntimado.pdf`);
-    }
-
-    // pdf open in a new tab
-    const pdfDataUri = pdf.output('datauristring');
-    const newTab = window.open();
-    newTab?.document.write(`<iframe width='100%' height='100%' src='${pdfDataUri}'></iframe>`);
+        // pdf open in a new tab
+        const pdfDataUri = pdf.output('datauristring');
+        const newTab = window.open();
+        newTab?.document.write(`<iframe width='100%' height='100%' src='${pdfDataUri}'></iframe>`);
+    });
 }
 
 export default PdfGenerator;
